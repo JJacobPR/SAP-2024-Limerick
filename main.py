96c7d3162d0f3a2e8f341b198d9c0f03f8b059c8
oliviagoo222 <oliviagoode78@gmail.com>
2024-06-25 15:48:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import pandas as pd
 import numpy as np
 from screens.add_expense import add_expenses
 from screens.view_expense import view_expense
+from screens.overview import overview_tab
 
 if 'expense_categories' not in st.session_state:
     st.session_state.expense_categories = []
 
-[viewExpensesTab, addExpensesTab] = st.tabs(['View Expenses', 'Add Expenses'])
+[viewExpensesTab, addExpensesTab, OverviewTab] = st.tabs(['View Expenses', 'Add Expenses', 'Overview Tab'])
 
 with viewExpensesTab:
     view_expense()
 
 with addExpensesTab: 
     add_expenses()
+
+with OverviewTab:
+    overview_tab()
